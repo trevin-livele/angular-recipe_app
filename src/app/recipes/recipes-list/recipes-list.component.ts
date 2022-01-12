77662b0b78789f7b4faf53283caf98766b023026
trevin-livele <liveletrevin6@gmail.com>
2022-01-12 13:14:51 +0300

    created content in the recipe component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/12/29/19/37/food-3048440_960_720.jpg')
+  ];
 
   constructor() { }
 
